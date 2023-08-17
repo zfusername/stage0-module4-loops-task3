860340395a7b5502bc44d77c2744237c97676700
zfusername <farrukh_ziyaev@epam.com>
2023-08-17 11:08:31 +0500

    change

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.loops.task3;
 public class AlphabetPrinter {
     public void printAlphabet() {
         char alphabet;
-        for(alphabet = 'A'; alphabet <= 'Z'; alphabet++){
+        for(alphabet = 'a'; alphabet <= 'z'; alphabet++){
             System.out.print(alphabet);
         }
     }
